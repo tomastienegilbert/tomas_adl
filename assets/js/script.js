2fe93a9b11812faa1a46195dbf650be4262b8662
Tomás Hurtado <tomashurtadolatorre@gmail.com>
2021-09-09 18:08:51 -0300

    se añade función para cambiar color de fondo del navbar al scrollear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Función para cambiar fondo de Navbar al Scrollear
 $(window).scroll(function() {
-    $('.bg-transparent, .nav-bg-scrolled').toggleClass("bg-transparent nav-bg-scrolled", $(this).scrollTop() > 200);
-})
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
+});
